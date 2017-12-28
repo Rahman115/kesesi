bba6ed763d7cca5e33f5decccc50c03258131eb1
abdurrahman <abdurrahmanariyansyah@gmail.com>
2017-12-29 05:34:08 +0700

    ADD
    file

--- administrator/modules/views/transaksi_online.view.php
+++ administrator/modules/views/transaksi_online.view.php
@@ -24,3 +24,41 @@
         </ul>
     </div>
 </div>
+
+<div class="row">
+
+    <div class="col-lg-12">
+        <div class="table-responsive">
+            <table class="teble table-hover data-table table-striped tablesorter">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Jurusan</th>
+                        <th>Jumlah Laki-laki</th>
+                        <th>Jumlah Perempuan</th>
+                        <th>Total Siswa</th>
+                        <th>Wakil Kelas</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $n = 1;
+                    foreach ($data['rooms'] AS $val) {
+                        $v = explode('.', $val->room);
+                        ?>
+                        <tr>
+                            <td><?php echo $n++; ?></td>
+                            <td><?php echo $val->room; ?></td>
+                            <td><?php echo $val->sum_l; ?></td>
+                            <td><?php echo $val->sum_p; ?></td>
+                            <td><?php echo $val->sum_all; ?></td>
+                            <td><?php echo $val->name; ?></td>
+                            <td><a href="<?php echo SITE_URL; ?>?page=transaksi&action=rooms&major=<?php echo $v[1] . "." . $v[2]; ?>&wl=<?php echo $v[0] . "." . $val->teacher_code; ?>" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
